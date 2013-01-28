89be1cf1b0b4fa1419ca64d82839aea2cd4e50b9
Cheng Sun <chengsun9@gmail.com>
2013-01-28 15:03:32 +0000

    CODE: remove tabs

--- demolib/program.cpp
+++ demolib/program.cpp
@@ -110,28 +110,28 @@ void ProgramTest::updateMeshBuf(const Mesh &mesh)
         }
     }
 
-	glGenBuffers(1, &vertBufId);
-	glGenBuffers(1, &idxBufId);
+    glGenBuffers(1, &vertBufId);
+    glGenBuffers(1, &idxBufId);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vertBufId);
-	glBufferData(GL_ARRAY_BUFFER, vertBuf.size() * sizeof(vertBuf[0]),
+    glBindBuffer(GL_ARRAY_BUFFER, vertBufId);
+    glBufferData(GL_ARRAY_BUFFER, vertBuf.size() * sizeof(vertBuf[0]),
                  &vertBuf[0], GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxBufId);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, idxBuf.size() * sizeof(idxBuf[0]),
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxBufId);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, idxBuf.size() * sizeof(idxBuf[0]),
                  &idxBuf[0], GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-	glGenVertexArrays(1, &vaoId);
-	glBindVertexArray(vaoId);
+    glGenVertexArrays(1, &vaoId);
+    glBindVertexArray(vaoId);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vertBufId);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxBufId);
+    glBindBuffer(GL_ARRAY_BUFFER, vertBufId);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxBufId);
 
-	glBindVertexArray(0);
+    glBindVertexArray(0);
 }
 
 void ProgramTest::draw() const
