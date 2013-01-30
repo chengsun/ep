daf1fcc5e874d9fbbcdc6fe446e0b4884f792626
Cheng Sun <chengsun9@gmail.com>
2013-01-30 22:42:23 +0000

    CODE: started implementing Matrix ops

--- include/types.h
+++ include/types.h
@@ -9,8 +9,8 @@
 typedef uint8_t U8;
 typedef uint16_t U16;
 typedef uint32_t U32;
-static const U32 U32_MIN = static_cast<U32>(0);
-static const U32 U32_MAX = ~static_cast<U32>(0);
+static constexpr U32 U32_MIN = static_cast<U32>(0);
+static constexpr U32 U32_MAX = ~static_cast<U32>(0);
 typedef int8_t S8;
 typedef int16_t S16;
 typedef int32_t S32;
@@ -19,31 +19,41 @@ typedef int32_t S32;
  * Vector common ops
  *****************************************************************************/
 
+#define VECTOR_DECL(VecType) \
+    float &operator[](const size_t i); \
+    constexpr float operator[](const size_t i); \
+    constexpr VecType operator+(const VecType &b) const; \
+    constexpr VecType operator-(const VecType &b) const; \
+    constexpr VecType operator*(float f) const; \
+    constexpr VecType operator/(float f) const; \
+    VecType &operator+=(const VecType &b); \
+    VecType &operator-=(const VecType &b); \
+    VecType &operator*=(float f);
+
 #define VECTOR_DEFS(VecType) \
-    static constexpr VecType sub(const VecType &a, const VecType &b) { \
+    inline constexpr VecType sub(const VecType &a, const VecType &b) { \
         return add(a, neg(b)); \
     } \
-    static constexpr float hypot2(const VecType &a) { \
+    inline constexpr float hypot2(const VecType &a) { \
         return dot(a, a); \
     } \
-    static constexpr float hypot2(const VecType &a, const VecType &b) { \
+    inline constexpr float hypot2(const VecType &a, const VecType &b) { \
         return hypot2(a - b); \
     } \
-    static constexpr float hypot(const VecType &a) { \
+    inline constexpr float hypot(const VecType &a) { \
         return sqrtf(hypot2(a)); \
     } \
-    static constexpr float hypot(const VecType &a, const VecType &b) { \
+    inline constexpr float hypot(const VecType &a, const VecType &b) { \
         return sqrtf(hypot2(a, b)); \
     } \
-    static constexpr VecType normalize(const VecType &a) { \
+    inline constexpr VecType normalize(const VecType &a) { \
         return scalediv(a, hypot(a)); \
     } \
-    inline float &VecType::operator[](const unsigned i) { \
-        ASSERTX(i < VecType::length()); \
+    inline float &VecType::operator[](const size_t i) { \
+        ASSERTX(i < VecType::size()); \
         return (reinterpret_cast<float *>(this))[i]; \
     } \
-    inline float const &VecType::operator[](const unsigned i) const { \
-        ASSERTX(i < VecType::length()); \
+    inline constexpr float VecType::operator[](const size_t i) { \
         return (reinterpret_cast<const float *>(this))[i]; \
     } \
     inline constexpr VecType VecType::operator+(const VecType &b) const { \
@@ -71,17 +81,6 @@ typedef int32_t S32;
         return *this; \
     }
 
-#define VECTOR_DECL(VecType) \
-    float &operator[](const unsigned i); \
-    float const &operator[](const unsigned i) const; \
-    constexpr VecType operator+(const VecType &b) const; \
-    constexpr VecType operator-(const VecType &b) const; \
-    constexpr VecType operator*(float f) const; \
-    constexpr VecType operator/(float f) const; \
-    VecType &operator+=(const VecType &b); \
-    VecType &operator-=(const VecType &b); \
-    VecType &operator*=(float f);
-
 
 /******************************************************************************
  * Vec2
@@ -91,26 +90,24 @@ struct Vec2
 {
     float x, y;
 
-    static constexpr unsigned length() {
-        return 2;
-    }
+    static constexpr unsigned size() { return 2; }
 
     VECTOR_DECL(Vec2)
 };
 
-static constexpr Vec2 add(const Vec2 &a, const Vec2 &b) {
+inline constexpr Vec2 add(const Vec2 &a, const Vec2 &b) {
     return {a.x+b.x, a.y+b.y};
 }
-static constexpr Vec2 neg(const Vec2 &a) {
+inline constexpr Vec2 neg(const Vec2 &a) {
     return {-a.x, -a.y};
 }
-static constexpr Vec2 scale(const Vec2 &a, float f) {
+inline constexpr Vec2 scale(const Vec2 &a, float f) {
     return {a.x*f, a.y*f};
 }
-static constexpr Vec2 scalediv(const Vec2 &a, float f) {
+inline constexpr Vec2 scalediv(const Vec2 &a, float f) {
     return {a.x/f, a.y/f};
 }
-static constexpr float dot(const Vec2 &a, const Vec2 &b) {
+inline constexpr float dot(const Vec2 &a, const Vec2 &b) {
     return a.x*b.x + a.y*b.y;
 }
 
@@ -125,29 +122,27 @@ struct Vec3
 {
     float x, y, z;
 
-    static constexpr unsigned length() {
-        return 3;
-    }
+    static constexpr unsigned size() { return 3; }
 
     VECTOR_DECL(Vec3)
 };
 
-static constexpr Vec3 add(const Vec3 &a, const Vec3 &b) {
+inline constexpr Vec3 add(const Vec3 &a, const Vec3 &b) {
     return {a.x+b.x, a.y+b.y, a.z+b.z};
 }
-static constexpr Vec3 neg(const Vec3 &a) {
+inline constexpr Vec3 neg(const Vec3 &a) {
     return {-a.x, -a.y, -a.z};
 }
-static constexpr Vec3 scale(const Vec3 &a, float f) {
+inline constexpr Vec3 scale(const Vec3 &a, float f) {
     return {a.x*f, a.y*f, a.z*f};
 }
-static constexpr Vec3 scalediv(const Vec3 &a, float f) {
+inline constexpr Vec3 scalediv(const Vec3 &a, float f) {
     return {a.x/f, a.y/f, a.z/f};
 }
-static constexpr float dot(const Vec3 &a, const Vec3 &b) {
+inline constexpr float dot(const Vec3 &a, const Vec3 &b) {
     return a.x*b.x + a.y*b.y + a.z*b.z;
 }
-static constexpr Vec3 cross(const Vec3 &a, const Vec3 &b) {
+inline constexpr Vec3 cross(const Vec3 &a, const Vec3 &b) {
     return {a.y*b.z - a.z*b.y,
             a.z*b.x - a.x*b.z,
             a.x*b.y - a.y*b.x};
@@ -163,30 +158,54 @@ struct Vec4
 {
     float x, y, z, w;
 
-    static constexpr unsigned length() {
-        return 4;
-    }
+    static constexpr unsigned size() { return 4; }
 
     VECTOR_DECL(Vec4)
 };
 
-static constexpr Vec4 add(const Vec4 &a, const Vec4 &b) {
+inline constexpr Vec4 add(const Vec4 &a, const Vec4 &b) {
     return {a.x+b.x, a.y+b.y, a.z+b.z, a.w+b.w};
 }
-static constexpr Vec4 neg(const Vec4 &a) {
+inline constexpr Vec4 neg(const Vec4 &a) {
     return {-a.x, -a.y, -a.z, -a.w};
 }
-static constexpr Vec4 scale(const Vec4 &a, float f) {
+inline constexpr Vec4 scale(const Vec4 &a, float f) {
     return {a.x*f, a.y*f, a.z*f, a.w*f};
 }
-static constexpr Vec4 scalediv(const Vec4 &a, float f) {
+inline constexpr Vec4 scalediv(const Vec4 &a, float f) {
     return {a.x/f, a.y/f, a.z/f, a.w/f};
 }
-static constexpr float dot(const Vec4 &a, const Vec4 &b) {
+inline constexpr float dot(const Vec4 &a, const Vec4 &b) {
     return a.x*b.x + a.y*b.y + a.z*b.z + a.w*b.w;
 }
 
 VECTOR_DEFS(Vec4)
+
+
+/******************************************************************************
+ * Matrix common ops
+ *****************************************************************************/
+
+#include <exception>
+
+#define MATRIX_DECL(MatType) \
+    static constexpr unsigned nRows() { \
+        return MatType::ColType::size(); \
+    } \
+    static constexpr unsigned size() { \
+        return nCols() * nRows(); \
+    } \
+    MatType::ColType &operator[](size_t i); \
+    constexpr MatType::ColType const &operator[](size_t i);
+
+#define MATRIX_DEFS(MatType) \
+    inline MatType::ColType &MatType::operator[](size_t i) { \
+        ASSERTX(i < MatType::nCols()); \
+        return v[i]; \
+    } \
+    inline constexpr MatType::ColType const &MatType::operator[](size_t i) { \
+        return v[i]; \
+    } \
 
 /******************************************************************************
  * Mat2
@@ -196,7 +215,36 @@ struct Mat2
 {
     typedef Vec2 ColType;
     ColType v[2];
+
+    static constexpr unsigned nCols() { return 2; }
+
+    MATRIX_DECL(Mat2)
 };
+
+inline constexpr Mat2 add(const Mat2 &a, const Mat2 &b) {
+    return {{add(a.v[0], b.v[0]),
+             add(a.v[1], b.v[1])}};
+}
+inline constexpr Mat2 neg(const Mat2 &a) {
+    return {{neg(a.v[0]),
+             neg(a.v[1])}};
+}
+inline constexpr Mat2 scale(const Mat2 &a, float f) {
+    return {{scale(a.v[0], f),
+             scale(a.v[1], f)}};
+}
+inline constexpr Mat2 scalediv(const Mat2 &a, float f) {
+    return {{scalediv(a.v[0], f),
+             scalediv(a.v[1], f)}};
+}
+inline constexpr Mat2 mul(const Mat2 &a, const Mat2 &b) {
+    return {{{a.v[0][0]*b.v[0][0] + a.v[1][0]*b.v[0][1],
+              a.v[0][1]*b.v[0][0] + a.v[1][1]*b.v[0][1]},
+             {a.v[0][0]*b.v[1][0] + a.v[1][0]*b.v[1][1],
+              a.v[0][1]*b.v[1][0] + a.v[1][1]*b.v[1][1]}}};
+}
+
+MATRIX_DEFS(Mat2)
 
 
 /******************************************************************************
